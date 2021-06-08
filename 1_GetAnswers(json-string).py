4bd9ec78d5a176438ddd44a0d2fd4a4fda1209d6
DaniellaDeLauri <64893185+DaniellaDeLauri@users.noreply.github.com>
2021-06-08 18:57:00 +0300

    Update 1_GetAnswers(json-string).py

--- 1_GetAnswers(json-string).py
+++ 1_GetAnswers(json-string).py
@@ -1,16 +1,14 @@
-#1-й кусок из 4-х кусков в плане Артёма
-import json
-
-with open("questionary-2.json", "r", encoding="utf-8-sig") as read_file:
-    raw_data = json.load(read_file)
-big_array = []
-
-for index in raw_data[0]['participations'].keys():
-    array = []
-    array.append(index)
-    if type(raw_data[0]['participations'][index]) == list:
-        for info_segment in raw_data[0]['participations'][index]:
-            if type(info_segment) == dict and 'questionId' in info_segment.keys() and 'answer' in info_segment.keys():
-                if info_segment['questionId'] == '1' or info_segment['questionId'] == '4':
-                    array.append(info_segment['answer'])
-    big_array.append(array)    
+injson = input()
+meow = injson.replace('[','').replace('{','').replace(']','').replace('}','').replace('"','').replace('answer','').replace('questionId:','').replace(':',',').replace(',,',',')
+end = ['','','','','','']
+ans = meow.split(',')
+for i in range(len(ans)):
+    try:
+        if int(ans[i]) > 5:
+            end[0] = ans[i]
+        else:
+            end[int(ans[i])+1] = ans[i+1]
+    except ValueError:
+        pass
+    #print(end)
+#в end хранится список сортированный
