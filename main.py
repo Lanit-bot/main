be999c32fc82509e6f1040417a039644bee7b4bf
chickysnail <48623496+chickysnail@users.noreply.github.com>
2021-06-09 14:14:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from GetAnswers import GetAnswers
+import json
 
-str = input()
-inp_list = GetAnswers(str)
+import GetAnswers
+import CreateVectorForPerson
+import FindSimilarPerson
 
+
+def checkJsonData(data: str):
+    try:
+        json.loads(data)
+    except:
+        return False
+    return True
+
+def main():
+    # {"132524162":[{"questionId":"1","answer":"\"Хористы; Чернобыль\""},{"questionId":"2","answer":"\"Дюна; Благие знамения\""},{"questionId":"3","answer":"ZHU; Asking Alexandria; Ляпис Трубецкой"},{"questionId":"4","answer":"Игра на муз.инструменте"},{"questionId":"0","answer":"\"Автомобили; Путешествия; Музыка\""}]}
+    newData = input()  # input json string
+    if not checkJsonData(newData):
+        raise TypeError
+    answersArray = GetAnswers.GetAnswers(newData)
+    vectorMatrix = CreateVectorForPerson.CreateVector(answersArray)
+    personIds = FindSimilarPerson.GetSimilarPerson(vectorMatrix)
+    return personIds
+
+
+if __name__ == '__main__':
+    main()
