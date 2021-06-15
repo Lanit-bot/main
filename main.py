5f28abce097106674a7c33012d6af4c60e1d28e1
DaniellaDeLauri <64893185+DaniellaDeLauri@users.noreply.github.com>
2021-06-15 12:44:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ def checkJsonData(data: str):
     return True
 
 
-def action():
+def action(newData):
     # "132524162":[{"questionId":"1","answer":"\"Хористы; Чернобыль\""},{"questionId":"2","answer":"\"Дюна; Благие знамения\""},{"questionId":"3","answer":"ZHU; Asking Alexandria; Ляпис Трубецкой"},{"questionId":"4","answer":"Игра на муз.инструменте"},{"questionId":"0","answer":"\"Автомобили; Путешествия; Музыка\""}]
-    newData = input()  # input json string
     if not checkJsonData(newData):
         raise TypeError
     answersArray = GetAnswers.GetAnswers(newData)
