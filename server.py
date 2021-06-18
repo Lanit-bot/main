0fc4616bfe312b0b01ff7f0227ae2dd59e8a417f
DaniellaDeLauri <64893185+DaniellaDeLauri@users.noreply.github.com>
2021-06-18 14:18:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,30 @@
-import fasttext
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from main import action
+from navec import Navec
+import processing_dataset
+from pydantic import BaseModel
+
 
 # App creation
 app = FastAPI()
 
+path = 'navec_hudlit_v1_12B_500K_300d_100q.tar'
+nana = Navec.load(path)
+pdt = processing_dataset.processing_dataset()
+
+class User(BaseModel):
+    interests: str
+    movies: str
+    books: str
+    music: str
+
+
 
 @app.post('/companions')
-def find_companions(user):
-    return action(user)
+def find_companions(user:User):
+    return action(user.interests, nana, pdt)
 
 
 if __name__ == '__main__':
